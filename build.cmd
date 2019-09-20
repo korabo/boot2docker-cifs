a6dc7476a4694409929185ed67730a9b9c760441
korabocintake <takeuchi@korabo.jp>
2019-09-20 18:03:10 +0900

    fix parameter, env def

--- build.cmd
+++ build.cmd
@@ -1,8 +1,10 @@
 :# create local user (e.g.  user=dockercifs/password=dockercifs)
 :# start C drive sharing only to given user (e.g. dockercifs)
-docker build --file Dockerfile --tag korabo/boot2docker:cifs ./
+docker build --file Dockerfile --tag korabo/boot2docker-cifs:latest ./
+:# docker login
+:# docker push korabo/boot2docker-cifs:latest
 :# docker run --env CIFS_USR=dockercifs --env CIFS_PSW=dockercifs --rm korabo/boot2docker-cifs:latest > boot2docker.cifs.iso
 :# sudo docker-machine create --driver hyperv --hyperv-virtual-switch hv-nat --hyperv-boot2docker-url boot2docker.cifs.iso dkcifs
-:# copied to C:\Users\s.takeuchi\.docker\machine\machines\default\boot2docker.iso
-:# AFTER CREATED: replace virtual-switch, docker-machine regenerate-certs dkcifs, docker-machine env dkcifs
-:# In boot2dockercifs ; sudo bash /opt/mnt_c.bash
+:# copied by docker-machine to C:\Users\<username>\.docker\machine\machines\default\boot2docker.iso
+:# AFTER CREATED: replace virtual-switch, docker-machine regenerate-certs default, docker-machine env default
+:# In boot2docker-cifs vm on Hyper-V  do: sudo bash /opt/mnt_c.bash
